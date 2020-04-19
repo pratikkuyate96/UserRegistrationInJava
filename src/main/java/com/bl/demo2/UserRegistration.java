fa0ffc1e52a5af671c96a8585982f5ced835d34f
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-19 19:33:04 +0530

    ADDED: Checked email id valid or not

--- src/main/java/com/bl/demo2/UserRegistration.java
+++ src/main/java/com/bl/demo2/UserRegistration.java
@@ -3,6 +3,7 @@ package com.bl.demo2;
 public class UserRegistration {
     public static final String PATTERN_OF_FIRSTNAME="^[A-Z][a-z]{2,}$";
     public static final String PATTERN_OF_LASTNAME="^[A-Z][a-z]{2,}$";
+    public static final String PATTERN_OF_EMAILID="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[a-zA-Z]{2,4}$";
 
     public boolean registerFirstName(String firstName)
     {
@@ -11,5 +12,9 @@ public class UserRegistration {
     public boolean registerLastName(String lastName)
     {
         return lastName.matches(PATTERN_OF_LASTNAME);
+    }
+    public boolean registerEmailId(String emailId)
+    {
+        return emailId.matches(PATTERN_OF_EMAILID);
     }
 }
--- src/test/java/com/bl/demo2/UserRegistrationTest.java
+++ src/test/java/com/bl/demo2/UserRegistrationTest.java
@@ -9,7 +9,7 @@ public class UserRegistrationTest
     public void registerFirstName_givenFirstName_shouldReturnTrue()
     {
         UserRegistration userregister = new UserRegistration();
-        boolean result = userregister.registerFirstName("Xyzz");
+        boolean result = userregister.registerFirstName("Pratik");
         System.out.println("First name : "+result );
         Assert.assertTrue(result);
     }
@@ -18,8 +18,17 @@ public class UserRegistrationTest
     public void registerLastName_givenLastName_shouldReturnTrue()
     {
         UserRegistration userregister = new UserRegistration();
-        boolean result = userregister.registerLastName("Abc");
+        boolean result = userregister.registerLastName("Kuyate");
         System.out.println("Last name : "+result );
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void registerEmailId_givenEmailId_shouldReturnTrue()
+    {
+        UserRegistration userregister = new UserRegistration();
+        boolean result = userregister.registerEmailId("pratik97@gmail.com");
+        System.out.println("Email Id : "+result );
         Assert.assertTrue(result);
     }
 }
